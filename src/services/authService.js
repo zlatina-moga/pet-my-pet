dfea750d1c0724c510459a48b16d5db9914e2575
Zlatina Moga <zlatina.delcheva@gmail.com>
2021-12-06 11:33:14 +0200

    added register

--- src/services/authService.js
+++ src/services/authService.js
@@ -17,6 +17,17 @@ export const login = async (email, password) => {
         throw jsonResult.message;
     }   
 }
+
+export const register = (email, password) => {
+    return fetch(`${baseUrl}/users/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email, password})
+    })
+        .then (res => res.json())
+}
     
 
 export const logout = () => {
