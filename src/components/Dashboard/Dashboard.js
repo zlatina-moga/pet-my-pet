13f4a65b7fc0f57332f91609d6d2bcedb4275508
Zlatina Moga <zlatina.delcheva@gmail.com>
2022-01-04 09:28:57 +0200

    updated dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,15 +1,12 @@
-import {Route, Routes} from 'react-router-dom';
 import PetList from '../PetList/PetList'
 
 export default function Dashboard() {
     return (
         <section id="dashboard-page" className="dashboard">
             <h1>Dashboard</h1>
-            <nav>
-                <Routes>
-                    <Route path='/' element={<PetList />}/>
-                </Routes>
-            </nav>   
+            <section>
+                <PetList />
+            </section>   
         </section>
     )
 }
